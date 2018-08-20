5be9d11e653cb4215d57aea1ec5740846ea78b8f
Cody <mizx@users.noreply.github.com>
2018-08-20 02:27:32 -0700

    added material-ui theme

--- packages/fortnite-ui/src/app.tsx
+++ packages/fortnite-ui/src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { theme } from './theme';
 
 const App: React.SFC = () => (
-  <>
+  <MuiThemeProvider theme={theme}>
     <CssBaseline />
-  </>
+  </MuiThemeProvider>
 );
 
 export default App;
---
+++ packages/fortnite-ui/src/theme.ts
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import amber from '@material-ui/core/colors/amber';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: amber
+  }
+});
