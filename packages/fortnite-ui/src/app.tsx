c64919adecf3f0cfa1cf36bce20e4722d9d9ca87
Cody <mizx@users.noreply.github.com>
2018-08-20 02:19:21 -0700

    added material-ui, allowed synthetic default imports, added app

---
+++ packages/fortnite-ui/src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App: React.SFC = () => (
+  <>
+    <CssBaseline />
+  </>
+);
+
+export default App;
--- packages/fortnite-ui/src/index.tsx
+++ packages/fortnite-ui/src/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-const App = () => <div>App</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
 ReactDOM.render(
   <App />,
