a73b6282eed0461cc79c75fe2943d92b0c5ab5c1
Cody <mizx@users.noreply.github.com>
2018-08-30 00:28:09 -0700

    refactored oauth gateway to only call two endpoints

--- packages/fortnite-api/src/gateways/oauth-gateway.ts
+++ packages/fortnite-api/src/gateways/oauth-gateway.ts
@@ -1,38 +1,73 @@
 import { get, post } from 'request-promise-native';
+import { Headers, CoreOptions as RequestOptions } from 'request';
 import { OAuthTokenResponse, OAuthExchangeResponse } from '../types/external';
 
 import { OAUTH_TOKEN_ENDPOINT, OAUTH_EXCHANGE_ENDPOINT } from '../urls.json';
+import { EPIC_AUTHORIZATION, FORTNITE_AUTHORIZATION } from '../config';
 
-const EPIC_AUTHORIZATION = 'MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y=';
-const FORTNITE_AUTHORIZATION = 'ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=';
+type AuthHeader = 'basic' | 'bearer';
+
+const defaultRequestOptions: RequestOptions = {
+  json: true
+};
+const defaultForm = {
+  includePerms: false
+};
+
+function getHeaders(token: string, type: AuthHeader = 'basic'): Headers {
+  return {
+    authorization: `${type} ${token}`
+  };
+}
+
+async function fetchToken(authorization: string, form: {}): Promise<OAuthTokenResponse> {
+  const headers = getHeaders(authorization);
+  const options = {
+    ...defaultRequestOptions,
+    headers,
+    form
+  };
+
+  return post(OAUTH_TOKEN_ENDPOINT, options);
+}
 
 export const fetchLauncherToken = async (username: string, password: string): Promise<OAuthTokenResponse> => {
-  const headers = { authorization: `basic ${EPIC_AUTHORIZATION}` };
-  const form = { username, password, grant_type: 'password', includePerms: false };
-  const options = { headers, form, json: true, url: OAUTH_TOKEN_ENDPOINT };
+  const form = {
+    username,
+    password,
+    grant_type: 'password'
+  };
 
-  return await post(options);
+  return fetchToken(EPIC_AUTHORIZATION, form);
 };
 
 export const fetchExchangeToken = async (accessToken: string): Promise<OAuthExchangeResponse> => {
-  const headers = { authorization: `bearer ${accessToken}` };
-  const options = { headers, json: true, url: OAUTH_EXCHANGE_ENDPOINT };
+  const headers = getHeaders(accessToken, 'bearer');
+  const options = {
+    ...defaultRequestOptions,
+    headers
+  };
 
-  return await get(options);
+  return await get(OAUTH_EXCHANGE_ENDPOINT, options);
 };
 
 export const fetchFortniteToken = async (exchangeCode: string): Promise<OAuthTokenResponse> => {
-  const headers = { authorization: `basic ${FORTNITE_AUTHORIZATION}` };
-  const form = { exchange_code: exchangeCode, grant_type: 'exchange_code', includePerms: false, token_type: 'eg1' };
-  const options = { headers, form, json: true, url: OAUTH_TOKEN_ENDPOINT };
+  const form = {
+    ...defaultForm,
+    exchange_code: exchangeCode,
+    grant_type: 'exchange_code',
+    token_type: 'eg1'
+  };
 
-  return await post(options);
+  return fetchToken(FORTNITE_AUTHORIZATION, form);
 };
 
 export const fetchRefreshToken = async (refreshToken: string): Promise<OAuthTokenResponse> => {
-  const headers = { authorization: `basic ${FORTNITE_AUTHORIZATION}` };
-  const form = { grant_type: 'refresh_token', refresh_token: refreshToken, includePerms: false };
-  const options = { headers, form, json: true, url: OAUTH_TOKEN_ENDPOINT };
+  const form = {
+    ...defaultForm,
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken
+  };
 
-  return await post(options);
+  return fetchToken(FORTNITE_AUTHORIZATION, form);
 };
